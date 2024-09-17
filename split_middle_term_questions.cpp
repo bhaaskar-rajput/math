881e1694adc4cb5d447e4f34b9ef9926b2696b2c
Bhaskar Rajput <139103642+bhaskar-rajput@users.noreply.github.com>
2024-09-17 10:33:58 +0530

    v1

--- split_middle_term_questions.cpp
+++ split_middle_term_questions.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
+
+struct quad_poly{
+    int p[3];
+    struct zeroes{
+        struct z1{
+
+        };
+        struct z2;
+    };
+
+};
+
 int main(){
+
     return 0;
 }
